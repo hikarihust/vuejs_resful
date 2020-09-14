afa68c8dbf7834403de29a57fa82074e5662c73c
vudinhquang <vudinhquangk53@gmail.com>
2020-09-15 00:09:32 +0900

    VueJs Props

--- vuejs_basic/vue/src/App.vue
+++ vuejs_basic/vue/src/App.vue
@@ -2,28 +2,34 @@
   <div id="app">
       <h2>{{ title }}</h2>
       <Navbar></Navbar>
-      <nav>
-        <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">Post</a></li>
-            <li><a href="">About</a></li>
-        </ul>
-      </nav>
+      <AllFriends :friends="friends"></AllFriends>
+      <OnlineFriends :friends="friends"></OnlineFriends>
   </div>
 </template>
 
 <script>
 
 import Navbar from './components/Navbar'
+import AllFriends from './components/AllFriends'
+import OnlineFriends from './components/OnlineFriends'
 export default {
   name: 'App',
   data() {
       return {
-          title: 'Wow Our vue website are running'
+          title: 'Wow Our vue website are running',
+          friends: [
+              { name: 'A', online: true },
+              { name: 'B', online: false },
+              { name: 'C', online: true },
+              { name: 'D', online: true },
+              { name: 'E', online: false },
+          ]
       }
   },
   components: {
-      Navbar
+      Navbar,
+      AllFriends,
+      OnlineFriends
   }
 }
 </script>
