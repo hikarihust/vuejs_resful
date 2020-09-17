e23ccce3225902e6e644d71673c7876449bad20b
vudinhquang <vudinhquangk53@gmail.com>
2020-09-18 00:22:18 +0900

    VueJs Router Object

--- vuejs_basic/mvue/src/components/Navbar.vue
+++ vuejs_basic/mvue/src/components/Navbar.vue
@@ -5,6 +5,7 @@
             <li><router-link to="/">Home</router-link></li>
             <li><router-link to="/about">About</router-link></li>
             <li><router-link to="/contact">Contact</router-link></li>
+            <li><router-link to="/profile/1">Profile(1)</router-link></li>
         </ul>
       </nav>
   </div>
