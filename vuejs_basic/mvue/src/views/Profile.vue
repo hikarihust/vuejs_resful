a1315d436fe70a65024b52cf0a4e9cf1c69f38b8
vudinhquang <vudinhquangk53@gmail.com>
2020-09-18 00:04:00 +0900

    VueJs Router Parameter

---
+++ vuejs_basic/mvue/src/views/Profile.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="profile">
+      <h2>Profile Page ({{ userId }})</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  components: {
+  },
+  data() {
+      return {
+          userId: this.$route.params.user_id
+      }
+  }
+}
+</script>
+
+<style scoped>
+    h2 {
+        color: green
+    }
+</style>
