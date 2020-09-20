b9dbca09d881559465d5a91e4c55782b88f8b6e1
vudinhquang <vudinhquangk53@gmail.com>
2020-09-21 03:15:22 +0900

    Project Helper User

--- laravel-vue-master/resources/js/app.js
+++ laravel-vue-master/resources/js/app.js
@@ -5,6 +5,10 @@ import VueRouter from 'vue-router'
 
 import router from './router'
 
+// Import User Class
+import User from './Helpers/User';
+window.User = User;
+
 Vue.use(VueRouter)
 
 const app = new Vue({
